050353151b7cc64b0b769dead980fdd2389acd52
Srikanth AD <adsrikanth@gmail.com>
2018-12-07 16:43:53 -0600

    updated deprecated listview in react native to use flatlist

--- src/components/SummaryList.js
+++ src/components/SummaryList.js
@@ -1,44 +1,30 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { ListView } from 'react-native';
+import { FlatList } from 'react-native';
+import { Spinner } from '../components/common';
 import { summariesFetch } from '../actions';
 import ListItem from './ListItem';
 
 class SummaryList extends Component {
-  componentWillMount() {
+
+  keyExtractor = (item) => item.uid;
+
+  componentDidMount() {
     this.props.summariesFetch();
-
-    this.createDataSource(this.props);
   }
 
-  componentWillReceiveProps(nextProps) {
-    // nextProps are the next set of props that this component
-    // will be rendered with
-    // this.props is still the old set of props
-
-    this.createDataSource(nextProps);
-  }
-
-  createDataSource({ summaries }) {
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
-
-    this.dataSource = ds.cloneWithRows(summaries);
-  }
-
-  renderRow(summary) {
-    return <ListItem summary={summary} />;
+  renderRow({item }) {
+    return <ListItem summary={item} {...this.props} />;
   }
 
   render() {
+    if (!this.props.summaries.length) return <Spinner size="large"/>;
     return (
-      <ListView
-        style={{backgroundColor: '#f3f3f3'}}
-        enableEmptySections
-        dataSource={this.dataSource}
-        renderRow={this.renderRow}
+      <FlatList
+          data={this.props.summaries}
+          keyExtractor={this.keyExtractor}
+          renderItem={this.renderRow.bind(this)}
       />
     );
   }
@@ -48,7 +34,7 @@ const mapStateToProps = state => {
   const summaries = _.map(state.summaries, (val, uid) => {
     return { ...val, uid };
   });
-
+  console.log(summaries);
   return { summaries };
 };
 
